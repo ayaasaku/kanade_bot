4816657419ff23fb929445f1635bf6376f5f9193
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-07-28 20:56:46 +0800

    fix

--- utility/apps/sekai/music_info.py
+++ utility/apps/sekai/music_info.py
@@ -10,31 +10,41 @@ async def get_music_title(music_id):
 async def get_music_lyricist(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_lyricist = music_api[music_id-1]['lyricist']
+    for ids in music_api:
+        if music_api[ids]['id'] == music_id:
+            music_title = music_api[ids]['lyricist']
     return music_lyricist
 
 async def get_music_composer(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_composer = music_api[music_id-1]['composer']
+    for ids in music_api:
+        if music_api[ids]['id'] == music_id:
+            music_composer =music_api[ids]['composer']
     return music_composer
 
 async def get_music_arranger(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_arranger = music_api[music_id-1]['arranger']
+    for ids in music_api:
+        if music_api[ids]['id'] == music_id:
+            music_arranger = music_api[ids]['arranger']
     return music_arranger
 
 async def get_music_published_time(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_published_time = music_api[music_id-1]['publishedAt']
+    for ids in music_api:
+        if music_api[ids]['id'] == music_id:
+            music_published_time = music_api[ids]['publishedAt']
     return music_published_time
 
 async def get_music_asset_name(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_asset_name = music_api[music_id-1]['assetbundleName']
+    for ids in music_api:
+        if music_api[ids]['id'] == music_id:
+            music_asset_name = music_api[ids]['assetbundleName']
     return music_asset_name
 #music_difficulties
 

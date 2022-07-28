b7ea5d78d5c2dbe9ef2bb39f78f8f7f393e6169a
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-07-28 21:30:05 +0800

    final fix!

--- utility/apps/sekai/music_info.py
+++ utility/apps/sekai/music_info.py
@@ -11,32 +11,46 @@ async def get_music_title(music_id):
 async def get_music_lyricist(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_lyricist = music_api[music_id-1]['lyricist']
-    return music_lyricist
+    for thing in music_api:
+        thing_id = thing['id']
+        if music_id == thing_id:
+            music_lyricist = thing['lyricist']
+            return music_lyricist
 
 async def get_music_composer(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_composer = music_api[music_id-1]['composer']
-    return music_composer
+    for thing in music_api:
+        thing_id = thing['id']
+        if music_id == thing_id:
+            music_composer = thing['composer']
+            return music_composer
 
 async def get_music_arranger(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_arranger = music_api[music_id-1]['arranger']
-    return music_arranger
-
+    for thing in music_api:
+        thing_id = thing['id']
+        if music_id == thing_id:
+            music_arranger = thing['arranger']
+            return music_arranger
+        
 async def get_music_published_time(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_published_time = music_api[music_id-1]['publishedAt']
-    return music_published_time
-
+    for thing in music_api:
+        thing_id = thing['id']
+        if music_id == thing_id:
+            music_published_time = thing['publishedAt']
+            return music_published_time    
+    
+'''
 async def get_music_asset_name(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
     music_asset_name = music_api[music_id-1]['assetbundleName']
     return music_asset_name
+'''
 #music_difficulties
 
 #music_vocals

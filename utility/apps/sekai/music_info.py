5e6f61d447e15dc6483ea58cf96584710c7f4f4f
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-07-28 16:16:46 +0800

    fix

--- utility/apps/sekai/music_info.py
+++ utility/apps/sekai/music_info.py
@@ -2,37 +2,37 @@
 async def get_music_title(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_title = music_api[1]['title']
+    music_title = music_api[2]['title']
     return music_title
 
 async def get_music_lyricist(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_lyricist = music_api[1]['lyricist']
+    music_lyricist = music_api[2]['lyricist']
     return music_lyricist
 
 async def get_music_composer(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_composer = music_api[1]['composer']
+    music_composer = music_api[2]['composer']
     return music_composer
 
 async def get_music_arranger(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_arranger = music_api[1]['arranger']
+    music_arranger = music_api[2]['arranger']
     return music_arranger
 
 async def get_music_published_time(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_published_time = music_api[1]['publishedAt']
+    music_published_time = music_api[2]['publishedAt']
     return music_published_time
 
 async def get_music_asset_name(music_id):
     from utility.apps.sekai.api_functions import get_sekai_musics_api
     music_api = await get_sekai_musics_api()
-    music_asset_name = music_api[1]['assetbundleName']
+    music_asset_name = music_api[2]['assetbundleName']
     return music_asset_name
 #music_difficulties
 

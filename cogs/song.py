c3205b43e4ab5245629a828fa5106862a6099525
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-07-28 22:08:44 +0800

    difficulty

--- cogs/song.py
+++ cogs/song.py
@@ -17,7 +17,12 @@ class SongCog(commands.Cog, name='song'):
     @app_commands.command(name='songs', description='get songs info')     
     @app_commands.rename(import_id='music_id')            
     async def event(self, interaction: discord.Interaction, import_id: int):
-        from utility.apps.sekai.music_info import get_music_title, get_music_lyricist, get_music_composer, get_music_arranger,get_music_published_time, get_music_asset_name
+        from utility.apps.sekai.music_info import get_music_title, get_music_lyricist, get_music_composer, get_music_arranger,get_music_published_time, \
+            get_music_difficulty_easy_difficulty, get_music_difficulty_easy_level, get_music_difficulty_easy_note_count, \
+            get_music_difficulty_normal_difficulty, get_music_difficulty_normal_level, get_music_difficulty_normal_note_count, \
+            get_music_difficulty_hard_difficulty, get_music_difficulty_hard_level, get_music_difficulty_hard_note_count, \
+            get_music_difficulty_expert_difficulty, get_music_difficulty_expert_level, get_music_difficulty_expert_note_count, \
+            get_music_difficulty_master_difficulty, get_music_difficulty_master_level, get_music_difficulty_master_note_count
         from utility.apps.sekai.time_formatting import format_date
         
         global music_id
@@ -34,6 +39,26 @@ class SongCog(commands.Cog, name='song'):
             music_asset_name = f'jacket_s_0{music_id}'
         elif len(str(music_id)) == 3:
             music_asset_name = f'jacket_s_{music_id}'
+            
+        easy_difficulty = get_music_difficulty_easy_difficulty(music_id)
+        easy_level = get_music_difficulty_easy_level(music_id)
+        easy_note_count = get_music_difficulty_easy_note_count(music_id)
+        
+        normal_difficulty = get_music_difficulty_normal_difficulty(music_id)
+        normal_level = get_music_difficulty_normal_level(music_id)
+        normal_note_count = get_music_difficulty_normal_note_count(music_id)
+        
+        hard_difficulty = get_music_difficulty_hard_difficulty(music_id)
+        hard_level = get_music_difficulty_hard_level(music_id)
+        hard_note_count = get_music_difficulty_hard_note_count(music_id)
+        
+        expert_difficulty = get_music_difficulty_expert_difficulty(music_id)
+        expert_level = get_music_difficulty_expert_level(music_id)
+        expert_note_count = get_music_difficulty_expert_note_count(music_id)
+        
+        master_difficulty = get_music_difficulty_master_difficulty(music_id)
+        master_level = get_music_difficulty_master_level(music_id)
+        master_note_count = get_music_difficulty_master_note_count(music_id)
 
         cover_url = f"https://minio.dnaroma.eu/sekai-assets/music/jacket/{music_asset_name}_rip/{music_asset_name}.webp"
         music_url = f'https://sekai.best/music/{music_id}'
@@ -44,6 +69,11 @@ class SongCog(commands.Cog, name='song'):
         embed.add_field(name='作曲', value=music_composer, inline=True)
         embed.add_field(name='編曲', value=music_arranger, inline=True)
         embed.add_field(name='發佈時間', value=music_published_time, inline=False)
+        embed.add_field(name=easy_difficulty, value=f'等級：{easy_level}\n音符數量：{easy_note_count}', inline=False)
+        embed.add_field(name=normal_difficulty, value=f'等級：{normal_level}\n音符數量：{normal_note_count}', inline=False)
+        embed.add_field(name=hard_difficulty, value=f'等級：{hard_level}\n音符數量：{hard_note_count}', inline=False)
+        embed.add_field(name=expert_difficulty, value=f'等級：{expert_level}\n音符數量：{expert_note_count}', inline=False)
+        embed.add_field(name=master_difficulty, value=f'等級：{master_level}\n音符數量：{master_note_count}', inline=False)
         embed.add_field(name='更多資訊', value=music_url, inline=False)
         #embed.add_field(name='\u200b', value='\u200b', inline=True)
         await interaction.response.send_message(embed=embed)
--- utility/apps/sekai/music_info.py
+++ utility/apps/sekai/music_info.py
@@ -52,5 +52,88 @@ async def get_music_asset_name(music_id):
     return music_asset_name
 '''
 #music_difficulties
+async def get_music_difficulty_easy_difficulty(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    difficulty = music_api[music_id-1]['musicDifficulty'].capitalize()
+    return difficulty
 
-#music_vocals
+async def get_music_difficulty_easy_level(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    level = music_api[music_id-1]['playLevel']
+    return level
+
+async def get_music_difficulty_easy_note_count(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    note_count = music_api[music_id-1]['noteCount']
+    return note_count
+
+async def get_music_difficulty_normal_difficulty(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    difficulty = music_api[music_id]['musicDifficulty'].capitalize()
+    return difficulty
+
+async def get_music_difficulty_normal_level(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    level = music_api[music_id]['playLevel']
+    return level
+
+async def get_music_difficulty_normal_note_count(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    note_count = music_api[music_id]['noteCount']
+    return note_count
+
+async def get_music_difficulty_hard_difficulty(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    difficulty = music_api[music_id+1]['musicDifficulty'].capitalize()
+    return difficulty
+
+async def get_music_difficulty_hard_level(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    level = music_api[music_id+1]['playLevel']
+    return level
+
+async def get_music_difficulty_hard_note_count(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    note_count = music_api[music_id+1]['noteCount']
+    return note_count
+
+async def get_music_difficulty_expert_difficulty(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    difficulty = music_api[music_id+3]['musicDifficulty'].capitalize()
+    return difficulty
+
+async def get_music_difficulty_expert_level(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    level = music_api[music_id+3]['playLevel']
+    return level
+
+async def get_music_difficulty_expert_note_count(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    note_count = music_api[music_id+3]['noteCount']
+    return note_count
+
+async def get_music_difficulty_master_difficulty(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    difficulty = music_api[music_id+3]['musicDifficulty'].capitalize()
+    return difficulty
+
+async def get_music_difficulty_master_level(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    music_api = await get_sekai_musics_api()
+    level = music_api[music_id+3]['playLevel']
+    return level
+
+async def get_music_difficulty_master_note_count(music_id):
+    from utility.apps.sekai.api_functions import get_sekai_musics_api
+    note_count = music_api[music_id+3]['noteCount']
+    return note_count
+

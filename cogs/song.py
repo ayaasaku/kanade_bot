9bfd668db953fe803f2287a40cf33116e198c695
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-07-28 15:55:55 +0800

    test

--- cogs/song.py
+++ cogs/song.py
@@ -30,11 +30,12 @@ class SongCog(commands.Cog, name='song'):
         music_published_time = await format_date(await get_music_published_time(music_id))
         music_asset_name = str(get_music_asset_name(music_id))
 
-        cover_url = f"https://minio.dnaroma.eu/sekai-assets/music/jacket/{music_asset_name}_rip/{music_asset_name}.webp"
+        #cover_url = f"https://minio.dnaroma.eu/sekai-assets/music/jacket/{music_asset_name}_rip/{music_asset_name}.webp"
+        cover_url = f"https://minio.dnaroma.eu/sekai-assets/music/jacket/jacket_s_257_rip/jacket_s_257.webp"
         music_url = f'https://sekai.best/music/{music_id}'
 
         embed = defaultEmbed(title=f'**{music_title}**')
-        #embed.set_thumbnail(url=cover_url)
+        embed.set_thumbnail(url=cover_url)
         embed.add_field(name='作詞', value=music_lyricist, inline=True)
         embed.add_field(name='作曲', value=music_composer, inline=True)
         embed.add_field(name='編曲', value=music_arranger, inline=True)

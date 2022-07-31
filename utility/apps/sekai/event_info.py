87eaa196b4366d62cea2a29acddb1bf8dda923e7
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-08-01 00:37:14 +0800

    fix

--- utility/apps/sekai/event_info.py
+++ utility/apps/sekai/event_info.py
@@ -111,3 +111,31 @@ async def get_event_bonus_attribute_tw():
     event_api = await get_sekai_event_deck_bonuses_api_tw()
     event_bonus_attribute = event_api[-1]['cardAttr'].capitalize()
     return event_bonus_attribute
+
+async def get_event_bonus_characters_id_tw(event_id):
+    from utility.apps.sekai.api_functions import get_sekai_event_deck_bonuses_api_tw
+    event_api = await get_sekai_event_deck_bonuses_api_tw()
+    characters_id_list = []
+    for thing in event_api:
+        if event_id == thing['eventId']:
+            character_id = thing.get('gameCharacterUnitId')
+            characters_id_list.append(character_id)
+    if None in characters_id_list:
+        characters_id_list.remove(None)
+    characters_id_list = list(dict.fromkeys(characters_id_list))
+    return characters_id_list
+
+async def get_event_bonus_characters_name_tw(characters_id_list: list):
+    from utility.apps.sekai.api_functions import get_sekai_characters_info_api
+    characters_name_list = []
+    event_api = await get_sekai_characters_info_api()
+    for character_id in characters_id_list:
+        for thing in event_api:
+            if character_id == thing['id']:
+                first_name = thing['firstName']
+                last_name = thing['givenName']
+                name = f'{first_name}{last_name}'
+                characters_name_list.append(name)
+    if None in characters_name_list:
+        characters_name_list.remove(None)
+    return characters_name_list   

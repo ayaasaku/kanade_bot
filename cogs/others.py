b27180a39ec023cb1f3686aa08fc59376a6d8dbc
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-07-31 16:47:44 +0800

    fix

--- cogs/others.py
+++ cogs/others.py
@@ -15,16 +15,21 @@ class OthersCog(commands.Cog, name='others'):
         self.bot = bot
 
     @app_commands.command(name='embed', description='embed')
-    @app_commands.rename(description='embed-description', cmd_1='cmd-1-name', cmd_1_des='cmd-1-description', cmd_2='cmd-2-name', cmd_2_des='cmd-2-description', cmd_3='cmd-3-name', cmd_3_des='cmd-3-description')
+    @app_commands.rename(
+        description='embed-description', \
+        cmd_1='cmd-1-name', cmd_1_des_ln_1='cmd-1-description', cmd_1_des_ln_2='cmd-1-description', \
+        cmd_2='cmd-2-name', cmd_2_des_ln_1='cmd-2-description', cmd_2_des_ln_2='cmd-2-description', \
+        cmd_3='cmd-3-name', cmd_3_des_ln_1='cmd-3-description', cmd_3_des_ln_2='cmd-3-description'
+        )
     @app_commands.checks.has_role('小雪團隊')
     async def update(self, interaction: discord.Interaction, description: str, cmd_1: str = '', cmd_1_des: str = '', cmd_2: str = '', cmd_2_des: str = '', cmd_3: str = '', cmd_3_des: str = ''):
-        embed = updateEmbed(description=description,)
+        embed = updateEmbed(description=description)
         if len(cmd_1) >= 1:
-            embed.add_field(name=cmd_1, value=cmd_1_des, inline=False)
+            embed.add_field(name=cmd_1, value=f'{cmd_1_des_ln_1}\n{cmd_1_des_ln_2}', inline=False)
         if len(cmd_2) >= 1:
-            embed.add_field(name=cmd_2, value=cmd_2_des, inline=False)
+            embed.add_field(name=cmd_2, value=f'{cmd_2_des_ln_1}\n{cmd_2_des_ln_2}', inline=False)
         if len(cmd_3) >= 1:
-            embed.add_field(name=cmd_3, value=cmd_3_des, inline=False)
+            embed.add_field(name=cmd_3, value=f'{cmd_3_des_ln_1}\n{cmd_3_des_ln_2}', inline=False)
         await interaction.response.send_message(embed=embed)
         
 async def setup(bot: commands.Bot) -> None:

a452201fe5e105c7d7dcb7bb63f2c2a8a31880f2
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-08-01 11:20:32 +0800

    fix

--- cogs/song.py
+++ cogs/song.py
@@ -16,36 +16,39 @@ class SongCog(commands.Cog, name='song'):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
-    class SongCog(SelectOption):
-        options = [SelectOption(label='虛擬歌手', description='バーチャル・シンガー', value=0), 
-                SelectOption(label='25點，Nightcord見。', description='25時、ナイトコードで。', value=1), 
-                SelectOption(label='Leo/need', description='Leo/need', value=2), 
-                SelectOption(label='MORE MORE JUMP！', description='MORE MORE JUMP！', value=3), 
-                SelectOption(label='Vivid BAD SQUAD', description='Vivid BAD SQUAD', value=4), 
-                SelectOption(label='Wonderlands×Showtime', description='ワンダーランズ×ショウタイム', value=5)
-                ]
-        super().__init__(placeholder='選擇歌曲分類', options=options)
+    class SelectOptions(SelectOption):
+        def __init__(self):
+            options = [SelectOption(label='虛擬歌手', description='バーチャル・シンガー', value=0), 
+                    SelectOption(label='25點，Nightcord見。', description='25時、ナイトコードで。', value=1), 
+                    SelectOption(label='Leo/need', description='Leo/need', value=2), 
+                    SelectOption(label='MORE MORE JUMP！', description='MORE MORE JUMP！', value=3), 
+                    SelectOption(label='Vivid BAD SQUAD', description='Vivid BAD SQUAD', value=4), 
+                    SelectOption(label='Wonderlands×Showtime', description='ワンダーランズ×ショウタイム', value=5)
+                    ]
+            super().__init__(placeholder='選擇歌曲分類', options=options)
             
-        @app_commands.command(name='songs', description='get songs info')     
-        async def song(self, i: discord.Interaction):  
-            if self.options == 0:
-                embeds = get_vocaloid_music()
-                await GeneralPaginator(i, embeds).start(embeded=True)
-            elif self.options == 1:
-                embeds = get_school_refusal_music()
-                await GeneralPaginator(i, embeds).start(embeded=True)
-            elif self.options == 2:
-                embeds = get_light_music_club_music()
-                await GeneralPaginator(i, embeds).start(embeded=True)
-            elif self.options == 3:
-                embeds = get_idol_music()
-                await GeneralPaginator(i, embeds).start(embeded=True) 
-            elif self.options == 4:
-                embeds = get_street_music()
-                await GeneralPaginator(i, embeds).start(embeded=True)
-            elif self.options == 5:
-                embeds = get_theme_park_music()
-                await GeneralPaginator(i, embeds).start(embeded=True)               
+    @app_commands.command(name='songs', description='get songs info')     
+    async def song(self, i: discord.Interaction):  
+        select_view = SongCog.SelectOptions()
+        await select_view.wait()
+        if self.options == 0:
+            embeds = get_vocaloid_music()
+            await GeneralPaginator(i, embeds).start(embeded=True)
+        elif self.options == 1:
+            embeds = get_school_refusal_music()
+            await GeneralPaginator(i, embeds).start(embeded=True)
+        elif self.options == 2:
+            embeds = get_light_music_club_music()
+            await GeneralPaginator(i, embeds).start(embeded=True)
+        elif self.options == 3:
+            embeds = get_idol_music()
+            await GeneralPaginator(i, embeds).start(embeded=True) 
+        elif self.options == 4:
+            embeds = get_street_music()
+            await GeneralPaginator(i, embeds).start(embeded=True)
+        elif self.options == 5:
+            embeds = get_theme_park_music()
+            await GeneralPaginator(i, embeds).start(embeded=True)               
 
     
 async def setup(bot: commands.Bot) -> None:

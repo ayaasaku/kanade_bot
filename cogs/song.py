2edf1d2422a8215bf5e71759dbc1c2f1a6c555e2
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-08-04 12:14:31 +0800

    fix

--- cogs/song.py
+++ cogs/song.py
@@ -14,41 +14,37 @@ from utility.apps.sekai.music_info import get_group_music
 
 class SongCog(commands.Cog, name='song'):
     def __init__(self, bot: commands.Bot):
-        super().__init__()
+        options = [SelectOption(label='虛擬歌手', description='バーチャル・シンガー', value=0), 
+                SelectOption(label='25點，Nightcord見。', description='25時、ナイトコードで。', value=1), 
+                SelectOption(label='Leo/need', description='Leo/need', value=2), 
+                SelectOption(label='MORE MORE JUMP！', description='MORE MORE JUMP！', value=3), 
+                SelectOption(label='Vivid BAD SQUAD', description='Vivid BAD SQUAD', value=4), 
+                SelectOption(label='Wonderlands×Showtime', description='ワンダーランズ×ショウタイム', value=5)
+                ]
+        super().__init__(placeholder='選擇歌曲分類', options=options)
         self.bot = bot
-    class SelectOptions(Select):
-        def __init__(self):
-            select = Select(options = [SelectOption(label='虛擬歌手', description='バーチャル・シンガー', value=0), 
-                    SelectOption(label='25點，Nightcord見。', description='25時、ナイトコードで。', value=1), 
-                    SelectOption(label='Leo/need', description='Leo/need', value=2), 
-                    SelectOption(label='MORE MORE JUMP！', description='MORE MORE JUMP！', value=3), 
-                    SelectOption(label='Vivid BAD SQUAD', description='Vivid BAD SQUAD', value=4), 
-                    SelectOption(label='Wonderlands×Showtime', description='ワンダーランズ×ショウタイム', value=5)
-                    ])
-            super().__init__(placeholder='選擇歌曲分類', select=select)
-            self.select = select
             
     @app_commands.command(name='songs', description='get songs info')     
     async def song(self, i: discord.Interaction):  
         view = View()
-        view.add_item(self.SelectOptions.select)
+        view.add_item(self.options)
         await i.send(view)
-        if self.SelectOptions.options == 0:
+        if self.value == 0:
             embeds = await get_group_music('vocaloid', self.bot.session)
             await GeneralPaginator(i, embeds).start(embeded=True)
-        elif self.SelectOptions.options == 1:
+        elif self.value == 1:
             embeds = await get_group_music('school_refusal', self.bot.session)
             await GeneralPaginator(i, embeds).start(embeded=True)
-        elif self.SelectOptions.options == 2:
+        elif self.value == 2:
             embeds = await get_group_music('light_music_club', self.bot.session)
             await GeneralPaginator(i, embeds).start(embeded=True)
-        elif self.SelectOptions.options == 3:
+        elif self.value == 3:
             embeds = await get_group_music('idol', self.bot.session)
             await GeneralPaginator(i, embeds).start(embeded=True) 
-        elif self.SelectOptions.options == 4:
+        elif self.value == 4:
             embeds = await get_group_music('street', self.bot.session)
             await GeneralPaginator(i, embeds).start(embeded=True)
-        elif self.SelectOptions.options == 5:
+        elif self.value == 5:
             embeds = await get_group_music('theme_park', self.bot.session)
             await GeneralPaginator(i, embeds).start(embeded=True)               
         

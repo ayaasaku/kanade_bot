412d91010ff5d2ec56e8abaceff4f6260ecee27b
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-08-04 12:04:39 +0800

    fix

--- cogs/song.py
+++ cogs/song.py
@@ -7,7 +7,7 @@ import time, re, discord
 
 from discord import (ButtonStyle, Interaction, Member, SelectOption,
                      app_commands)
-from discord.ui import Button, Modal, Select, TextInput
+from discord.ui import Button, Modal, Select, TextInput, View
 from utility.utils import defaultEmbed
 from utility.paginators import GeneralPaginator
 from utility.apps.sekai.music_info import get_group_music
@@ -18,18 +18,20 @@ class SongCog(commands.Cog, name='song'):
         self.bot = bot
     class SelectOptions(Select):
         def __init__(self):
-            options = [SelectOption(label='虛擬歌手', description='バーチャル・シンガー', value=0), 
+            select = Select(optionsoptions = [SelectOption(label='虛擬歌手', description='バーチャル・シンガー', value=0), 
                     SelectOption(label='25點，Nightcord見。', description='25時、ナイトコードで。', value=1), 
                     SelectOption(label='Leo/need', description='Leo/need', value=2), 
                     SelectOption(label='MORE MORE JUMP！', description='MORE MORE JUMP！', value=3), 
                     SelectOption(label='Vivid BAD SQUAD', description='Vivid BAD SQUAD', value=4), 
                     SelectOption(label='Wonderlands×Showtime', description='ワンダーランズ×ショウタイム', value=5)
-                    ]
-            super().__init__(placeholder='選擇歌曲分類', options=options)
+                    ])
+            super().__init__(placeholder='選擇歌曲分類', select=select)
             
     @app_commands.command(name='songs', description='get songs info')     
     async def song(self, i: discord.Interaction):  
-        select_view = SongCog.SelectOptions()
+        view = View()
+        view.add_item(self.select)
+        await i.send(view)
         if self.SelectOptions.options == 0:
             embeds = await get_group_music('vocaloid', self.bot.session)
             await GeneralPaginator(i, embeds).start(embeded=True)
@@ -48,7 +50,7 @@ class SongCog(commands.Cog, name='song'):
         elif self.SelectOptions.options == 5:
             embeds = await get_group_music('theme_park', self.bot.session)
             await GeneralPaginator(i, embeds).start(embeded=True)               
-        return select_view
+        
     
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(SongCog(bot))

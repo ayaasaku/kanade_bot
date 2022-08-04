51bf3ce5e316574773c64b8c7c2922a98654e8d2
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-08-04 14:14:33 +0800

    fix

--- cogs/song.py
+++ cogs/song.py
@@ -30,46 +30,46 @@ class SongCog(commands.Cog, name='song'):
     @app_commands.command(name='songs', description='get songs info')  
   
     async def song(self, interaction: discord.Interaction): 
-        embeds = await get_group_music('vocaloid', self.bot.session)
-        await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True)
-        '''select = Select(placeholder='選擇歌曲分類', options = [SelectOption(label='虛擬歌手', description='バーチャル・シンガー'), 
+        select = Select(placeholder='選擇歌曲分類', options = [SelectOption(label='虛擬歌手', description='バーチャル・シンガー'), 
                     SelectOption(label='25點，Nightcord見。', description='25時、ナイトコードで。'), 
                     SelectOption(label='Leo/need', description='Leo/need'), 
                     SelectOption(label='MORE MORE JUMP！', description='MORE MORE JUMP！'), 
                     SelectOption(label='Vivid BAD SQUAD', description='Vivid BAD SQUAD'), 
                     SelectOption(label='Wonderlands×Showtime', description='ワンダーランズ×ショウタイム')
                     ])
+        
+        view = View()
+        view.add_item(select)
+        await interaction.response.send_message(view=view) 
+        
         async def song_callback(interaction: discord.Interaction):  
             #await i.response.send_message(f'{select.values[0]}')
             if select.values[0] == '虛擬歌手':
-                #embed = await get_song_embed(1, self.bot.session)
                 embeds = await get_group_music('vocaloid', self.bot.session)
                 await interaction.response.send_message(embed=embeds[0])
                 #await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True)
             elif select.values[0] == '25點，Nightcord見。':
                 embeds = await get_group_music('school_refusal', self.bot.session)
-                await GeneralPaginator(i, embeds).start(embeded=True)
-                await i.response.send_message('success')
+                await interaction.response.send_message(embed=embeds[0])
+                #await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True)
             elif select.values[0] == 'Leo/need':
                 embeds = await get_group_music('light_music_club', self.bot.session)
-                await GeneralPaginator(i, embeds).start(embeded=True)
-                await i.response.send_message('success')
+                await interaction.response.send_message(embed=embeds[0])
+                #await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True)
             elif select.values[0] == 'MORE MORE JUMP！':
                 embeds = await get_group_music('idol', self.bot.session)
-                await GeneralPaginator(i, embeds).start(embeded=True) 
-                await i.response.send_message('success')
+                await interaction.response.send_message(embed=embeds[0])
+                #await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True)
             elif select.values[0] == 'Vivid BAD SQUAD':
                 embeds = await get_group_music('street', self.bot.session)
-                await GeneralPaginator(i, embeds).start(embeded=True)
-                await i.response.send_message('success')
+                await interaction.response.send_message(embed=embeds[0])
+                #await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True)
             elif select.values[0] == 'Wonderlands×Showtime':
                 embeds = await get_group_music('theme_park', self.bot.session)
-                await GeneralPaginator(i, embeds).start(embeded=True)
-                await i.response.send_message('success')
+                await interaction.response.send_message(embed=embeds[0])
+                #await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True)
         select.callback = song_callback
-        view = View()
-        view.add_item(select)
-        await interaction.response.send_message(view=view) '''
+        
               
         
     

fc00475ff26afc35cb1f9d7c690f86044ad1e296
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-08-05 13:42:58 +0800

    fix

--- cogs/song.py
+++ cogs/song.py
@@ -43,8 +43,8 @@ class SongCog(commands.Cog, name='song'):
             #await i.response.send_message(f'{select.values[0]}')
             if select.values[0] == '虛擬歌手':
                 embeds = await get_group_music('vocaloid', self.bot.session)
-                await interaction.followup.send(embed=embeds[0])
-                #await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True)
+                #await interaction.followup.send(embed=embeds[0])
+                await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True)
             '''if select.values[0] == '25點，Nightcord見。':
                 #embeds = await get_group_music('school_refusal', self.bot.session)
                 #await interaction.response.defer()

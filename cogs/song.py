8254189d6091d9ef91b2214144eb39a38cd061c2
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-08-22 19:45:53 +0800

    fix

--- cogs/song.py
+++ cogs/song.py
@@ -38,16 +38,17 @@ class SongCog(commands.Cog, name='song'):
                           
         
         async def song_callback(interaction: discord.Interaction):  
-            await interaction.response.defer(ephemeral=True)
             if select.values[0] == '虛擬歌手':
                 embed = await loading_embed('虛擬歌手', 'vocaloid')
-                await interaction.followup.send(embed=embed, ephemeral=True)
+                await interaction.response.send_message(embed=embed, ephemeral=True)
                 embeds = await get_group_music('vocaloid', self.bot.session)
+                await interaction.response.defer()
                 await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True, edit_original_message= True)
                 await interaction.delete_original_response()
             if select.values[0] == '25點，Nightcord見。':
                 embed = await loading_embed(group = '25點，Nightcord見。', group_id = 'school_refusal')
-                await interaction.followup.send(embed = embed, )
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+                await interaction.response.defer()
                 embeds = await get_group_music('school_refusal', self.bot.session)
                 await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True, edit_original_message= True)
             if select.values[0] == 'Leo/need':

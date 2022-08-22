63ea90aa9a97315ab318db00f2a2221a023e1266
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-08-22 19:42:19 +0800

    fix

--- cogs/song.py
+++ cogs/song.py
@@ -38,12 +38,13 @@ class SongCog(commands.Cog, name='song'):
                           
         
         async def song_callback(interaction: discord.Interaction):  
-            await interaction.response.defer(ephemeral=True, delete = 15.0)
+            await interaction.response.defer(ephemeral=True)
             if select.values[0] == '虛擬歌手':
                 embed = await loading_embed('虛擬歌手', 'vocaloid')
-                await interaction.followup.send(embed=embed, ephemeral=True, delete = 15.0)
+                await interaction.followup.send(embed=embed, ephemeral=True)
                 embeds = await get_group_music('vocaloid', self.bot.session)
                 await GeneralPaginator(interaction, embeds).start(embeded=True, follow_up=True, edit_original_message= True)
+                await interaction.delete_original_response()
             if select.values[0] == '25點，Nightcord見。':
                 embed = await loading_embed(group = '25點，Nightcord見。', group_id = 'school_refusal')
                 await interaction.followup.send(embed = embed, )

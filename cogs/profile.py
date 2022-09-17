cb90dc668fb18d7a127dc56345fca9fb24aa511c
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-17 09:14:03 +0800

    fix

--- cogs/profile.py
+++ cogs/profile.py
@@ -11,7 +11,7 @@ class ProfileCog(commands.Cog, name='profile'):
     def __init__(self, bot):
         self.bot = bot
         
-    @app_commands.command(name='profile', description='查看一個玩家的帳戶')    
+    '''@app_commands.command(name='profile', description='查看一個玩家的帳戶')    
     #@app_commands.rename(user_id='玩家id')           
     async def profile(self, interaction: discord.Interaction):#, user_id: int): 
         interaction.response.defer()
@@ -34,7 +34,7 @@ class ProfileCog(commands.Cog, name='profile'):
              
             asyncio.sleep(0.01)
             interaction.response.send_message(embed=embed)
-            interaction.followup.send(embed=embed)
+            interaction.followup.send(embed=embed)'''
         
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(ProfileCog(bot))

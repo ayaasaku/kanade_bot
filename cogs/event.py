27771f52e4c70c9b65ebefaa14201afc549d2d0c
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-17 09:12:36 +0800

    try

--- cogs/event.py
+++ cogs/event.py
@@ -1,4 +1,4 @@
-import time, discord
+import time, discord,asyncio
 from discord import app_commands
 from discord.ext import commands
 from discord.app_commands import Choice
@@ -10,6 +10,8 @@ from data.translate_data import translate
 from utility.apps.sekai.time_formatting import format_time, format_date_jp, format_date, format_progress
 
 from utility.apps.sekai.event_info import get_event_info_jp, get_event_info_tw
+from utility.apps.sekai.user.profile import user_profile
+from utility.apps.sekai.user.data_processing import *
 
 class EventCog(commands.Cog, name='event'):
     def __init__(self, bot):
@@ -102,6 +104,31 @@ class EventCog(commands.Cog, name='event'):
             #embed.add_field(name='\u200b', value='**時間**', inline=False)
             embed.add_field(name='更多資訊', value=event_url, inline=False)
             await interaction.response.send_message(embed=embed)
+            
+            @app_commands.command(name='profile', description='查看一個玩家的帳戶')    
+            #@app_commands.rename(user_id='玩家id')           
+            async def profile(self, interaction: discord.Interaction):#, user_id: int): 
+                interaction.response.defer()
+                user_id = 244775114281091084 
+                if user_id == await get_user_game_data(user_id, 'userId', self.bot.session):
+                    name = await get_user_game_data(user_id, 'name', self.bot.session)
+                    id = user_id
+                    rank = await get_user_game_data(user_id, 'rank', self.bot.session)
+                    word = await get_user_profile(user_id, 'word', self.bot.session)
+                    twitter_id = await get_user_profile(user_id, 'twitterId', self.bot.session) 
+                    leader_id = await get_user_decks(user_id, 'leader', self.bot.session)
+                    img_url = await get_user_profile_pic(user_id, leader_id, self.bot.session)       
+                    
+                    embed = defaultEmbed(title=f'**{name}**', description=f'「{word}」')
+                    embed.set_thumbnail(url=img_url)
+                    embed.add_field(name=f'等級：', value=f'{rank}', inline=False)
+                    if twitter_id != None or len(twitter_id) >= 1: 
+                        embed.add_field(name=f'Twitter：', value=f'{twitter_id}', inline=False)
+                    embed.add_field(name=f'Id：', value=f'`{id}`', inline=False)
+                    
+                    asyncio.sleep(0.01)
+                    interaction.response.send_message(embed=embed)
+                    interaction.followup.send(embed=embed)
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(EventCog(bot))

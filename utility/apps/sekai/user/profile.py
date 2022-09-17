d0082209ff157616baffcdde2c138a0177e509b6
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-17 14:53:39 +0800

    fix

--- utility/apps/sekai/user/profile.py
+++ utility/apps/sekai/user/profile.py
@@ -11,25 +11,20 @@ from utility.utils import defaultEmbed
 async def user_profile(import_id: int, session: aiohttp.ClientSession):
     user_id = 244775114281091084 
     if user_id == await get_user_game_data(import_id, 'userId', session):
-        #name = await get_user_game_data(import_id, 'name', session)
+        name = await get_user_game_data(import_id, 'name', session)
         id = user_id
-        #rank = await get_user_game_data(import_id, 'rank', session)
-        #word = await get_user_profile(import_id, 'word', session)
-        #twitter_id = await get_user_profile(import_id, 'twitterId', session) 
-        #leader_id = await get_user_decks(import_id, 'leader', session)
-        #img_url = await get_user_profile_pic(import_id, leader_id, session)       
-        
-        name = 'ayaakaa'
-        rank = 27
-        word = 'hi'
-        twitter_id = 123
-        img_url = 'https://asset.pjsekai.moe/startapp/thumbnail/chara/res017_no012_after_training.png'
+        rank = await get_user_game_data(import_id, 'rank', session)
+        word = await get_user_profile(import_id, 'word', session)
+        twitter_id = await get_user_profile(import_id, 'twitterId', session) 
+        leader_id = await get_user_decks(import_id, 'leader', session)
+        img_url = await get_user_profile_pic(import_id, leader_id, session)       
+    
         
         if word == None or len(word) < 1: word = 'none'
         
         embed = defaultEmbed(title=f'**{name}**', description=f'「{word}」')
         embed.set_thumbnail(url=img_url)
-        embed.add_field(name=f'等級：', value=f'{rank}', inline=False)
+        embed.add_field(name=f'等級：', value=rank, inline=False)
         if twitter_id != None or len(twitter_id) >= 1: 
             embed.add_field(name=f'Twitter：', value=f'{twitter_id}', inline=False)
         embed.add_field(name=f'Id：', value=f'`{id}`', inline=False)

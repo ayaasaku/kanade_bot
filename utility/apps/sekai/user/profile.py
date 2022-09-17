357448031135a951e7419ea4e57406d35081439f
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-17 15:04:14 +0800

    fix

--- utility/apps/sekai/user/profile.py
+++ utility/apps/sekai/user/profile.py
@@ -15,18 +15,14 @@ async def user_profile(import_id: int, session: aiohttp.ClientSession):
         id = user_id
         rank = await get_user_game_data(import_id, 'rank', session)
         word = await get_user_profile(import_id, 'word', session)
-        twitter_id = await get_user_profile(import_id, 'twitterId', session) 
         leader_id = await get_user_decks(import_id, 'leader', session)
         img_url = await get_user_profile_pic(import_id, leader_id, session)       
-
         if word == None or len(word) < 1: word = 'none'
         
         embed = defaultEmbed(title=f'**{name}**', description=f'「{word}」')
         embed.set_thumbnail(url=img_url)
         embed.add_field(name=f'等級：', value=rank, inline=False)
-        if twitter_id == None or len(str(twitter_id)) < 1: twitter_id = 'none'
-        embed.add_field(name=f'Twitter：', value=f'{twitter_id}', inline=False)
-        embed.add_field(name=f'Id：', value=f'`{id}`', inline=False)
+        embed.add_field(name=f'玩家 ID：', value=f'`{id}`', inline=False)
 
         return embed
     

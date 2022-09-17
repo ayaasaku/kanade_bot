6a81e2f90a93c92fedf4aa059b1f9eaac11af6a8
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-17 09:14:36 +0800

    fix

--- cogs/event.py
+++ cogs/event.py
@@ -105,30 +105,30 @@ class EventCog(commands.Cog, name='event'):
             embed.add_field(name='更多資訊', value=event_url, inline=False)
             await interaction.response.send_message(embed=embed)
             
-            @app_commands.command(name='profile', description='查看一個玩家的帳戶')    
-            #@app_commands.rename(user_id='玩家id')           
-            async def profile(self, interaction: discord.Interaction):#, user_id: int): 
-                interaction.response.defer()
-                user_id = 244775114281091084 
-                if user_id == await get_user_game_data(user_id, 'userId', self.bot.session):
-                    name = await get_user_game_data(user_id, 'name', self.bot.session)
-                    id = user_id
-                    rank = await get_user_game_data(user_id, 'rank', self.bot.session)
-                    word = await get_user_profile(user_id, 'word', self.bot.session)
-                    twitter_id = await get_user_profile(user_id, 'twitterId', self.bot.session) 
-                    leader_id = await get_user_decks(user_id, 'leader', self.bot.session)
-                    img_url = await get_user_profile_pic(user_id, leader_id, self.bot.session)       
-                    
-                    embed = defaultEmbed(title=f'**{name}**', description=f'「{word}」')
-                    embed.set_thumbnail(url=img_url)
-                    embed.add_field(name=f'等級：', value=f'{rank}', inline=False)
-                    if twitter_id != None or len(twitter_id) >= 1: 
-                        embed.add_field(name=f'Twitter：', value=f'{twitter_id}', inline=False)
-                    embed.add_field(name=f'Id：', value=f'`{id}`', inline=False)
-                    
-                    asyncio.sleep(0.01)
-                    interaction.response.send_message(embed=embed)
-                    interaction.followup.send(embed=embed)
+    @app_commands.command(name='profile', description='查看一個玩家的帳戶')    
+    #@app_commands.rename(user_id='玩家id')           
+    async def profile(self, interaction: discord.Interaction):#, user_id: int): 
+        interaction.response.defer()
+        user_id = 244775114281091084 
+        if user_id == await get_user_game_data(user_id, 'userId', self.bot.session):
+            name = await get_user_game_data(user_id, 'name', self.bot.session)
+            id = user_id
+            rank = await get_user_game_data(user_id, 'rank', self.bot.session)
+            word = await get_user_profile(user_id, 'word', self.bot.session)
+            twitter_id = await get_user_profile(user_id, 'twitterId', self.bot.session) 
+            leader_id = await get_user_decks(user_id, 'leader', self.bot.session)
+            img_url = await get_user_profile_pic(user_id, leader_id, self.bot.session)       
+            
+            embed = defaultEmbed(title=f'**{name}**', description=f'「{word}」')
+            embed.set_thumbnail(url=img_url)
+            embed.add_field(name=f'等級：', value=f'{rank}', inline=False)
+            if twitter_id != None or len(twitter_id) >= 1: 
+                embed.add_field(name=f'Twitter：', value=f'{twitter_id}', inline=False)
+            embed.add_field(name=f'Id：', value=f'`{id}`', inline=False)
+            
+            asyncio.sleep(0.01)
+            interaction.response.send_message(embed=embed)
+            interaction.followup.send(embed=embed)
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(EventCog(bot))

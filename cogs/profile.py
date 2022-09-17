da96327f8bcd9f4919622f08096a1baa4b2eafbe
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-17 11:26:57 +0800

    fix

--- cogs/profile.py
+++ cogs/profile.py
@@ -1,5 +1,5 @@
 import time, discord, asyncio
-from discord import app_commands, Interaction
+from discord import app_commands
 from discord.ext import commands
 from discord.app_commands import Choice
 
@@ -11,13 +11,12 @@ class ProfileCog(commands.Cog, name='profile'):
     def __init__(self, bot):
         self.bot = bot
         
-@app_commands.command(name='profile', description='查看一個玩家的帳戶')    
-#@app_commands.rename(user_id='玩家id')           
-async def profile(self, interaction: discord.Interaction):#, user_id: int): 
-    interaction.response.defer()
-    user_id = 244775114281091084
-    embed = await user_profile(user_id, self.bot.session)
-    interaction.followup.send(embed=embed)
+    @app_commands.command(name='profile', description='查看一個玩家的帳戶')    
+    async def profile(self, interaction: discord.Interaction):
+        interaction.response.defer()
+        user_id = 244775114281091084
+        embed = await user_profile(user_id, self.bot.session)
+        interaction.followup.send(embed=embed)
         
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(ProfileCog(bot))

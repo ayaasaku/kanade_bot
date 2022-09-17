39fdf77564d7b5374d81090a8d53ef4836509244
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-17 09:19:04 +0800

    fix

--- cogs/event.py
+++ cogs/event.py
@@ -109,26 +109,11 @@ class EventCog(commands.Cog, name='event'):
     #@app_commands.rename(user_id='玩家id')           
     async def profile(self, interaction: discord.Interaction):#, user_id: int): 
         #interaction.response.defer()
-        user_id = 244775114281091084 
-        if user_id == await get_user_game_data(user_id, 'userId', self.bot.session):
-            name = await get_user_game_data(user_id, 'name', self.bot.session)
-            id = user_id
-            rank = await get_user_game_data(user_id, 'rank', self.bot.session)
-            word = await get_user_profile(user_id, 'word', self.bot.session)
-            twitter_id = await get_user_profile(user_id, 'twitterId', self.bot.session) 
-            leader_id = await get_user_decks(user_id, 'leader', self.bot.session)
-            img_url = await get_user_profile_pic(user_id, leader_id, self.bot.session)       
-            
-            embed = defaultEmbed(title=f'**{name}**', description=f'「{word}」')
-            embed.set_thumbnail(url=img_url)
-            embed.add_field(name=f'等級：', value=f'{rank}', inline=False)
-            if twitter_id != None or len(twitter_id) >= 1: 
-                embed.add_field(name=f'Twitter：', value=f'{twitter_id}', inline=False)
-            embed.add_field(name=f'Id：', value=f'`{id}`', inline=False)
-            
-            #asyncio.sleep(0.01)
-            interaction.response.send_message(embed=embed)
-            #interaction.followup.send(embed=embed)
+        user_id = 244775114281091084
+        embed = await get_user_profile(user_id, self.bot.session)
+        #asyncio.sleep(0.01)
+        interaction.response.send_message(embed=embed)
+        #interaction.followup.send(embed=embed)
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(EventCog(bot))
--- utility/apps/sekai/user/profile.py
+++ utility/apps/sekai/user/profile.py
@@ -9,25 +9,22 @@ from utility.apps.sekai.user.data_processing import (get_user_game_data, get_use
 from utility.utils import defaultEmbed
 
 async def user_profile(import_id: int, session: aiohttp.ClientSession):
-    if import_id == await get_user_game_data(import_id, 'userId', session):
+    user_id = 244775114281091084 
+    if user_id == await get_user_game_data(import_id, 'userId', session):
         name = await get_user_game_data(import_id, 'name', session)
-        id = import_id
+        id = user_id
         rank = await get_user_game_data(import_id, 'rank', session)
         word = await get_user_profile(import_id, 'word', session)
         twitter_id = await get_user_profile(import_id, 'twitterId', session) 
         leader_id = await get_user_decks(import_id, 'leader', session)
         img_url = await get_user_profile_pic(import_id, leader_id, session)       
-    
-        if word == None: word = '404Error'
         
-        #embed = defaultEmbed(title=f'**{name}**', description=f'「{word}」')
-        #embed.set_thumbnail(url=img_url)
-        #embed.add_field(name=f'等級：', value=f'{rank}', inline=False)
-        #if twitter_id != None: embed.add_field(name=f'Twitter：', value=f'{twitter_id}', inline=False)
-        #embed.add_field(name=f'Id：', value=f'`{id}`', inline=False)
-        
-        #return embed
-        
-        message = [name, id, rank, word, twitter_id, img_url]
-        return message
+        embed = defaultEmbed(title=f'**{name}**', description=f'「{word}」')
+        embed.set_thumbnail(url=img_url)
+        embed.add_field(name=f'等級：', value=f'{rank}', inline=False)
+        if twitter_id != None or len(twitter_id) >= 1: 
+            embed.add_field(name=f'Twitter：', value=f'{twitter_id}', inline=False)
+        embed.add_field(name=f'Id：', value=f'`{id}`', inline=False)
+
+        return embed
     

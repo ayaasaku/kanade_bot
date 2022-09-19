ec344c6cc2bac7d1ff9aed2060c7d48002543920
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-19 08:42:43 +0800

    added creation date

--- utility/apps/sekai/user/profile.py
+++ utility/apps/sekai/user/profile.py
@@ -6,22 +6,25 @@ from discord.app_commands import Choice
 import aiohttp
 
 from utility.apps.sekai.user.data_processing import (get_user_game_data, get_user_profile, get_user_decks, get_user_profile_pic)
+from utility.apps.sekai.time_formatting import format_date_jp
 from utility.utils import defaultEmbed
 
 async def user_profile(import_id: int, session: aiohttp.ClientSession):
     if import_id == await get_user_game_data(import_id, 'userId', session):
         name = await get_user_game_data(import_id, 'name', session)
-        id = import_id
+        user_id = import_id
         rank = await get_user_game_data(import_id, 'rank', session)
         word = await get_user_profile(import_id, 'word', session)
         leader_id = await get_user_decks(import_id, 'leader', session)
         img_url = await get_user_profile_pic(import_id, leader_id, session)       
         if word == None or len(word) < 1: word = 'none'
+        creation_date = format_date_jp(1600218000000 + user_id / 2 ** 22)
         
         embed = defaultEmbed(title=f'**{name}**', description=f'「{word}」')
         embed.set_thumbnail(url=img_url)
         embed.add_field(name=f'等級：', value=rank, inline=False)
-        embed.add_field(name=f'玩家 ID：', value=f'`{id}`', inline=False)
+        embed.add_field(name=f'玩家 ID：', value=f'`{user_id}`', inline=False)
+        embed.add_field(name=f'創建日期：', value=f'{creation_date}', inline=False)
 
         return embed
     

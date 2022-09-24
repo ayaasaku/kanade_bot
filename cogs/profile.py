af653a658cae8b93a530ae8e0e8b30db01001c31
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-24 10:14:55 +0800

    fix

--- cogs/profile.py
+++ cogs/profile.py
@@ -16,9 +16,9 @@ class ProfileCog(commands.Cog, name='profile'):
     async def profile(self, interaction: discord.Interaction, user_id: int = 244775114281091084):
         await interaction.response.defer()
         embed = loadingEmbed(text = '玩家', img = 'https://static.wikia.nocookie.net/projectsekai/images/b/bb/Yoisaki_Kanade_chibi.png/revision/latest?cb=20220320041840', thumbnail = True)
-        await interaction.followup.send(embed=embed)
-        embed = await user_profile(user_id, self.bot.session)
         await interaction.followup.send(embed=embed, ephemeral=True)
+        embed = await user_profile(user_id, self.bot.session)
+        await interaction.followup.send(embed=embed)
         
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(ProfileCog(bot))

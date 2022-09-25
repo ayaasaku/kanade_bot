523c54aefee282dd4debf58c97ef10b61b45a314
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-25 22:35:19 +0800

    fix

--- utility/apps/sekai/user/register.py
+++ utility/apps/sekai/user/register.py
@@ -1,17 +1,21 @@
-from sqlite3 import dbapi2
+import aiosqlite
 from utility.utils import errEmbed
 
-async def check_user_account(self, discord_id: int):
-    global db
-    db = self.bot.db
-    cursor = await db.cursor()
-    await cursor.execute('SELECT discord_id FROM user_accounts WHERE discord_id = ?', (discord_id,))
-    result = await cursor.fetchone()
-    if result is None:
-        return False
-    else:
-        return True
-    
-    ''' embed = errEmbed(
-            '找不到帳號！',
-            f'<@{discord_id}>\n請使用  `/register`  創建一個帳號後再重新執行操作')'''
+class RegisterApp:
+    def __init__(self, db: aiosqlite.Connection) -> None:
+        self.db = db
+        
+    async def check_user_account(self, discord_id: int):
+        global db
+        db = self.db
+        cursor = await db.cursor()
+        await cursor.execute('SELECT discord_id FROM user_accounts WHERE discord_id = ?', (discord_id,))
+        result = await cursor.fetchone()
+        if result is None:
+            return False
+        else:
+            return True
+        
+        ''' embed = errEmbed(
+                '找不到帳號！',
+                f'<@{discord_id}>\n請使用  `/register`  創建一個帳號後再重新執行操作')'''

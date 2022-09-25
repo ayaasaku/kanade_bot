89efb8f5a7ed5e56f63e908a1e57a7ff6b71280c
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-25 22:31:27 +0800

    fix

--- utility/apps/sekai/user/register.py
+++ utility/apps/sekai/user/register.py
@@ -8,9 +8,10 @@ async def check_user_account(self, discord_id: int):
     await cursor.execute('SELECT discord_id FROM user_accounts WHERE discord_id = ?', (discord_id,))
     result = await cursor.fetchone()
     if result is None:
-        embed = errEmbed(
-            '找不到帳號！',
-            f'<@{discord_id}>\n請使用  `/register`  創建一個帳號後再重新執行操作')
-        return False, embed
+        return False
     else:
-        return True, None
+        return True
+    
+    ''' embed = errEmbed(
+            '找不到帳號！',
+            f'<@{discord_id}>\n請使用  `/register`  創建一個帳號後再重新執行操作')'''

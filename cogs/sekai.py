1904c7e3adc1339eec876f41244a38922a9a3ac7
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-28 14:50:44 +0800

    fix

--- cogs/sekai.py
+++ cogs/sekai.py
@@ -69,8 +69,8 @@ class SekaiCog(commands.Cog, name='sekai'):
         await cursor.execute('SELECT player_id from user_accounts WHERE discord_id = ?', (str(interaction.user.id),))
         player_id = await cursor.fetchone()
         player_id = int(player_id[0])
-        embed = loadingEmbed(text = '玩家', img = 'https://static.wikia.nocookie.net/projectsekai/images/b/bb/Yoisaki_Kanade_chibi.png/revision/latest?cb=20220320041840', thumbnail = True)
-        await interaction.followup.send(embed=embed, ephemeral=True)
+        loading_embed = loadingEmbed(text = '玩家', img = 'https://static.wikia.nocookie.net/projectsekai/images/b/bb/Yoisaki_Kanade_chibi.png/revision/latest?cb=20220320041840', thumbnail = True)
+        await interaction.followup.send(embed=loading_embed, ephemeral=True)
         embed = await user_profile(player_id, self.bot.session)
         await interaction.followup.send(embed=embed)
         

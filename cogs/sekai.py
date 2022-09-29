d57e6bd07659ba3993d663998384712af7b7e296
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-09-29 13:33:45 +0800

    fix

--- cogs/sekai.py
+++ cogs/sekai.py
@@ -1,3 +1,4 @@
+from types import NoneType
 import aiosqlite
 
 import discord
@@ -79,5 +80,5 @@ class SekaiCog(commands.Cog, name='sekai'):
         embed = await user_profile(player_id, self.bot.session)
         await interaction.followup.send(embed=embed)
         
-async def setup(bot: commands.Bot):
+async def setup(bot: commands.Bot) -> NoneType:
     await bot.add_cog(SekaiCog(bot))

87ff717d42ae3087417405a154ba841fca1cd9d2
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-12-08 14:08:57 +0800

    fix

--- utility/apps/sekai/user/data_processing.py
+++ utility/apps/sekai/user/data_processing.py
@@ -251,7 +251,7 @@ async def get_user_music(import_id: str, music_id, music_title, session: aiohttp
                             embed.add_field(name=f'Full Perfect',
                                             value=f'<:crosss:1024577482290114630> ', inline=True)
                 embed_list.append(embed)
-                return embed_list
+            return embed_list
                 
 async def get_user_music_options(session: aiohttp.ClientSession):
         api = await get_sekai_musics_api(session)

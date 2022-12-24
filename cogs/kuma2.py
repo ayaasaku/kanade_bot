35c8207b9ec51fe1be535e393518cc3209437507
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-12-24 11:27:56 +0800

    fix

--- cogs/kuma2.py
+++ cogs/kuma2.py
@@ -1,5 +1,5 @@
 import discord
-from discord import app_commands, client, Interaction, utils, Client, guild, Guild, Member
+from discord import Member
 from discord.ext import commands
 
 from utility.utils import is_ayaakaa, defaultEmbed
@@ -8,8 +8,8 @@ class Kuma2Cog(commands.Cog, name='kuma2'):
     def __init__(self, bot):
         self.bot = bot
         
-    @Client.event
-    async def on_member_join(member):
+    @commands.Cog.listener()
+    async def on_member_join(member: Member):
         if member.guild.id == 1000043106164539453: #1001466950309924876:
             #await member.add_roles([1001478249773289562,], reason = 'New Member', atomic = True)
             await member.add_roles([1056048151125037098,], reason = 'New Member', atomic = True)

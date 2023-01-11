8f23242087c5149c1649756d31cd0ede6dfa4bff
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2023-01-11 13:58:03 +0800

    fix

--- sekai/user/profile/profile.py
+++ sekai/user/profile/profile.py
@@ -2,6 +2,30 @@ import aiohttp
 from sekai.sekai_modules.main import get_data
 class UserProfile:
     async def __init__(self, user_id: str, session: aiohttp.ClientSession):
+        data = await get_data(server='jp', type='api', path=f'/user/{user_id}/profile', session=session)
+        self.userId = data['user']['userGamedata']['userId']
+        if int(user_id) != self.userId: raise SystemError('API error')
+        self.name = data['user']['userGamedata']['name']
+        self.deck = data['user']['userGamedata']['deck']
+        self.rank = data['user']['userGamedata']['rank']
+        try: self.word = data['userProfile']['word']
+        except: self.word = None
+        try: self.twitterId = data['userProfile']['twitterId']
+        except: self.twitterId = None
+        self.profileImageType = data['userProfile']['profileImageType']
+        self.userDecks = data['userDecks'][0]
+        self.userCards = data['userCards']
+        self.userMusics = data['userMusics']
+        self.userCharacters = data['userCharacters']
+        self.userChallengeLiveSoloResults = data['userChallengeLiveSoloResults']
+        self.userChallengeLiveSoloStages = data['userChallengeLiveSoloStages']
+        self.userAreaItems = data['userAreaItems']
+        self.userHonors = data['userHonors']
+        self.userBondsHonors = data['userBondsHonors']
+        self.userMusicResults = data['userMusicResults']
+        self.userCustomProfileCards = data['userCustomProfileCards']
+        self.userProfileHonors = data['userProfileHonors']
+        '''
         #user['userGamedata']
         self.userId = 0
         self.name = ''
@@ -37,7 +61,7 @@ class UserProfile:
         self.userProfileHonors = []
         
         await self.get_profile(user_id=user_id, session=session)
-        
+        '''
 
     async def get_profile(self, user_id: str, session: aiohttp.ClientSession):    
         data = await get_data(server='jp', type='api', path=f'/user/{user_id}/profile', session=session)

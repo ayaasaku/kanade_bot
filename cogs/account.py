8cc82fe5d06a0e9bee4b02107e17fe1006059f86
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2023-02-01 09:17:30 +0800

    fix

--- cogs/account.py
+++ cogs/account.py
@@ -85,7 +85,7 @@ class AccountCog(commands.Cog, name='account'):
     @app_commands.choices(option=[
         Choice(name='jp', value='jp'),
         Choice(name='tw', value='tw')])  
-    async def register(self, option: str, interaction: Interaction):
+    async def register(self, option: str, interaction: discord.Interaction):
         self.register_server = option
         db = await aiosqlite.connect("kanade_data.db")
         check = await check_user_account(discord_id = str(interaction.user.id), db=db, server=option)
